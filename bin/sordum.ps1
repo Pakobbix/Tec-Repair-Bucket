bc3c9f7950af983980c10f77b9896a38c210747d
Pakobbix <Pakobbix@gmail.com>
2020-09-12 22:54:01 +0200

    Aufräumen

--- bin/sordum.ps1
+++
@@ -1,6 +0,0 @@
-Join-Path $PSScriptRoot '..\bucket' -Resolve | Get-ChildItem | Where-Object {
-    $dsc = (Get-Content $_.FullName -Raw | ConvertFrom-Json).description
-    return ($dsc -like '*sordum*') -or ($dsc -like 'Kaspersky*')
-} | ForEach-Object {
-    & "$PSScriptRoot\checkver.ps1" $_.BaseName -Force
-}
